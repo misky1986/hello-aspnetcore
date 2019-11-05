7c77c13d8246640b016e8fa637987c0d4d0fe92e
misky1986 <mskydt86@gmail.com>
2019-11-05 23:29:59 +0100

    Moved gitignore and made test in controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace HelloAspNetCore.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "Burning"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
